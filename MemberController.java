527578740e936e971ea7045c6bd7df91c3f7a626
156root <37989557+156root@users.noreply.github.com>
2018-04-15 17:35:26 +0800

    2221232132
    22222312321

---
+++ MemberController.java
@@ -0,0 +1,37 @@
+package org.resort.controller;
+
+import org.resort.entity.Member;
+import org.resort.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/member")
+@Controller
+public class MemberController {
+    @Autowired
+    MemberService service;
+
+    /**
+     * 11111111111
+     * dsfafdsafdfdsaf
+     * 获取全部会员魂牵梦萦sfsdfdsfd
+     * @param map 存储对象
+     * @return 全部会员
+     */
+    @RequestMapping("/getAllMember.html")
+    public String getAllMember(Map<String,Object> map) {
+        /*Map<String, Object> map = new HashMap<String, Object>();*/
+        List<Member> memberList= service.getAllMember();
+        for (Member member : memberList) {
+            System.out.println("id:"+member.getId());
+            System.out.println("account:"+member.getAccount());
+        }
+        map.put("member", memberList);
+        return "member/show";
+    }
+}
